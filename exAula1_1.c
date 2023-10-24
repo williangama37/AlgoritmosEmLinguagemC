c5f631111787a43a3f8113215761517ff886fb51
williangama37 <146802088+williangama37@users.noreply.github.com>
2023-10-24 15:41:03 -0300

    Update exAula1_1.c

--- exAula1_1.c
+++ exAula1_1.c
@@ -4,4 +4,5 @@ int main(){
 	printf("Hello, world!\n");
 	system("pause");
 	//Este comando permite ler a mensagem ao executar no Windows, pois o programa só conclui a execução o comando do usuário
+	return 0;
 }
