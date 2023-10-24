9ecd6f9c9b3c7bc808ec2723b178d07ac02ea4d6
williangama37 <146802088+williangama37@users.noreply.github.com>
2023-10-24 13:00:32 -0300

    Create exAula1_1.c

---
+++ exAula1_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	printf("Hello, world!\n");
+	system("pause");
+	//Este comando permite ler a mensagem ao executar no Windows, pois o programa só conclui a execução o comando do usuário
+}
